40ac1aadaefc608336488172aa2a5336b0a02677
Victor <juarezv1990@gmail.com>
2023-01-04 19:59:44 -0500

    Blotter schema has not been created

--- pages/Blotter.jsx
+++ pages/Blotter.jsx
@@ -3,10 +3,10 @@ import { GraphQLClient, gql } from 'graphql-request'
 
 
 const Blotter = () => {
-    
+
   return (
     <div className='w-full'>
-        <div className='w-full h-[25vh]'></div>
+        <div className='w-full h-[25vh] relative'><span className='text-[25px] absolute bottom-[20px] left-[25%] border-b-2'>Another MTG Blog</span></div>
         <div className='max-w-[1240px] w-full  h-[80vh] mx-auto'>
             
         </div>
